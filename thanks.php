a74d454d94d2759d65e96e530ba85a2b7f17d29a
Marina Maric <masamana333@gmail.com>
2023-03-22 21:21:54 +0100

    Revert "add ppost method et validation"
    This reverts commit 7546109248336f79fb68c1c86098f4995b507eaa.

--- thanks.php
+++ thanks.php
@@ -1,11 +1,11 @@
 <?php
 
-$lastname = $_POST['lastname'];
+$name = $_POST['name'];
 $firstname = $_POST['firstname'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $subjects = $_POST['subjects'];
-$message = $_POST['message'];
+$message = $_POST['user_message'];
 
 ?>
 
@@ -21,19 +21,17 @@ $message = $_POST['message'];
 </head>
 
 <body>
-    <main class="container">
+    <div class="container bg-light border rounded p-5">
+        <h1>Merci pour votre message !</h1>
+        <p>Merci <?= $name . " " .  $firstname ?> de nous avoir contacté à propos de <?= $subjects ?></p>
 
-        <div class="container bg-light border rounded p-5">
-            <h1>Merci pour votre message !</h1>
-            <p>Merci <?= $firstname . " " .  $lastname ?> de nous avoir contacté à propos de <?= $subjects ?></p>
+        <p>Un de nos conseillers vous contactera soit à l’adresse <?= $email ?> ou par téléphone au <?= $phone ?> dans les plus brefs délais pour traiter votre demande : </p>
 
-            <p>Un de nos conseillers vous contactera soit à l’adresse <?= $email ?> ou par téléphone au <?= $phone ?> dans les plus brefs délais pour traiter votre demande : </p>
+        <p><?= $message ?></p>
 
-            <p><?= $message ?></p>
+        <h3 class="text-center mt-5">Nous allons vous recontacter très vite !</h3>
+    </div>
 
-            <h3 class="text-center mt-5">Nous allons vous recontacter très vite !</h3>
-        </div>
-    </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
